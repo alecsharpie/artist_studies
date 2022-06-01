a7fa10134892669b4133ffb2dbfaed071c65f5c5
Alec Sharp <alecsharpie@gmail.com>
2022-06-02 03:07:52 +1000

    add artloader module and start w show_xy_images in image_viz module

---
+++ artist_studies/image_viz.py
@@ -0,0 +1,20 @@
+import matplotlib.pyplot as plt
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from tensorflow.image import resize
+from tensorflow.keras.utils import img_to_array
+from tqdm import tqdm
+import numpy as np
+
+
+def show_xy_images(x_coords, y_coords, img_list, image_zoom=1):
+    fig, ax = plt.subplots(1,1, figsize=(16, 16))
+    artists = []
+    for x_coord, y_coord, img in tqdm(zip(x_coords, y_coords, img_list)):
+        c_img = resize(img_to_array(img), [50,50]) / 255.0
+        img = OffsetImage(c_img, zoom=image_zoom)
+        ab = AnnotationBbox(img, (x_coord, y_coord),
+                            xycoords='data',
+                            frameon=False)
+        artists.append(ax.add_artist(ab))
+    ax.update_datalim(np.stack(x_coords, y_coords))
+    ax.autoscale()
---
+++ artist_studies/loader.py
@@ -0,0 +1,95 @@
+import os
+import numpy as np
+from tensorflow.keras.utils import load_img, img_to_array
+from tensorflow.image import resize
+from PIL import Image
+from tqdm import tqdm
+
+
+class ArtLoader:
+    """Load in art from artists as an array, batched and ready for tf"""
+
+    def __init__(self):
+        pass
+
+    def get_image_single(self, path_to_image):
+        """read in a single image, as an np array with dataset dim"""
+        img = load_img(path_to_image).convert('RGB')
+        return np.expand_dims(img, 0)
+
+    def get_all_from_artist(self, artist_name):
+        """read in images from the prompts:
+        'A beautiful painting of a waterlily pond, {artist}, Trending on artstation',
+        'A beautiful painting of a building in a serene landscape, {artist}, Trending on artstation'
+        for a particluar artist"""
+        all_img_paths = os.listdir(f'{self.data_path}/{artist_name}')
+        pond_img_list = [
+            load_img(f'{self.data_path}/{artist_name}/{img_path}').convert(
+                'RGB') for img_path in all_img_paths
+            if img_path.endswith('png')
+        ]
+        return np.array(pond_img_list)
+
+    def get_image_folders(self,
+                          data_path,
+                          exclude_list,
+                          num_artists=None,
+                          preprocessor=None,
+                          shape=(224, 224)):
+        """set the path to the data directory
+        loop through all artists and load images
+        optionally process, eg for clip"""
+        self.data_path = data_path
+        self.preprocessor = preprocessor
+
+        # invert scale
+        #scale = 1 / scale
+
+        self.art_list = []
+        self.array_art_list = []
+        self.preprocessed_art_list = []
+        self.artist_list = []
+
+        if self.data_path[-1] == '/':
+            self.data_path = self.data_path[:-1]
+
+        exclude_list.append('.DS_Store')
+
+        if 'batch' in self.data_path:
+            self.all_artists_paths = [
+                artist for artist in os.listdir(self.data_path)
+                if artist not in exclude_list
+            ]
+        else:
+            self.all_artists_paths = []
+            batches = os.listdir(self.data_path)
+            for batch in batches:
+                for artist_path in os.listdir(f'{self.data_path}/{batch}'):
+                    if artist_path not in exclude_list:
+                        self.all_artists_paths.append(f'{batch}/{artist_path}')
+
+        if num_artists:
+            self.all_artists_paths = self.all_artists_paths[:num_artists]
+        for artist in tqdm(self.all_artists_paths):
+
+            for art in self.get_all_from_artist(artist):
+                # new_width = int(art.size[0] / scale)
+                # new_height = int(art.size[1] / scale)
+
+                # which is faster resize array or resize image
+
+                self.artist_list.append(artist)
+                self.array_art_list.append(
+                    resize(img_to_array(art), [shape[0], shape[1]]))
+                if preprocessor:
+                    img = self.preprocessor(
+                        art.resize([shape[0], shape[1]], Image.ANTIALIAS))
+                    self.preprocessed_art_list.append(img)
+                else:
+                    self.art_list.append(art)
+
+        if preprocessor:
+            self.preprocessed_art_list = np.stack(self.preprocessed_art_list)
+        else:
+            self.artist_list = np.array(self.artist_list, dtype=object)
+        self.array_art_list = np.array(self.array_art_list)
